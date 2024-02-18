dc0218353a0fa39ba0825c9c625386d8a83e1ef6
Chingiz Zhanarbaev <chingizzhanarbaev@gmail.com>
2024-02-18 23:24:59 +0300

    Revert "Update workflows"
    This reverts commit ebcc0d38b1157c839f86e3c4fd00fa1f1153b4c4.

--- setup.py
+++ setup.py
@@ -15,17 +15,14 @@ setup(
         "utm",
         "tqdm",
     ],
-    version='0.1.4',
+    version='0.1.3',
     description='A package for public transit routing and analysis',
     author='chingiztob',
 )
 
 #python setup.py sdist bdist_wheel
-
 #twine upload dist/*
 #twine upload --repository testpypi dist/*
-
 #pip install --index-url https://test.pypi.org/simple/ TransitTest
-#pip install --upgrade --index-url https://test.pypi.org/simple/ TransitTest
 
 #pip install --index-url https://test.pypi.org/simple/ transit
