5e216afa207f6ec6d71181190626c94f2324efc2
Chingiz Zhanarbaev <chingizzhanarbaev@gmail.com>
2024-02-22 12:05:20 +0300

    More fixing for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,4 +33,4 @@ source_suffix = ".rst"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
+html_static_path = []
