80f49c81d0c007ddc5f0d68819bb9c894d3b2f3b
Chingiz Zhanarbaev <chingizzhanarbaev@gmail.com>
2024-03-12 00:25:17 +0300

    PyPi release

--- nxtransit/__init__.py
+++ nxtransit/__init__.py
@@ -1,14 +1,12 @@
 # ruff: noqa: F401
 """
 NxTransit is a Python package designed for creating a multimodal graph representation of a public transportation systems. 
-It leverages General Transit Feed Specification (GTFS) data to construct the graph and perform various time-dependent calculations.
+It uses General Transit Feed Specification (GTFS) data to construct the graph and perform various time-dependent calculations.
 
 Key Features:
 - Multimodal Graph Creation: NxTransit can generate a graph that integrates different modes of transportation with street network.
 - Time-Dependent Calculations: The package allows for the analysis of transit dynamics by considering the time-dependency of transit schedules. This includes calculating shortest paths with departure times, travel time matrices, and service frequency.
 - GTFS Data Support: NxTransit uses GTFS data, a common format for public transportation schedules and associated geographic information, as the basis for graph construction and analysis.
-
-This package is primarily intended for use by researchers in the field of geography, transportation and urban studies.
 """
 from ._version import __version__ 
 
